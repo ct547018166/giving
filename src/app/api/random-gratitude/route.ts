a87fddc8c5519bcd0f8c9205d819a8782ee662aa
tony <547018166@qq.com>
2025-11-29 12:47:57 +0800

    优化抽奖逻辑

--- src/app/api/random-gratitude/route.ts
+++ src/app/api/random-gratitude/route.ts
@@ -4,10 +4,8 @@ import db from '@/lib/db';
 export async function GET() {
   try {
     // 直接从所有记录中随机选出一条，这样记录越多的人中奖概率越大
-    // 排除 '陈统'
     const gratitude = db.prepare(`
       SELECT * FROM gratitudes 
-      WHERE nickname != '陈统'
       ORDER BY RANDOM() 
       LIMIT 1
     `).get();
